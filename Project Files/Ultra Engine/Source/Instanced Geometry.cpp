cd556392cbdc10e0899b11b005dad1aa327a211c
Leadwerks Software <support@leadwerks.com>
2022-11-25 11:12:54 +0100

    adding project files

---
+++ Project Files/Ultra Engine/Source/Instanced Geometry.cpp
@@ -0,0 +1,56 @@
+#include "UltraEngine.h"
+
+using namespace UltraEngine;
+
+int main(int argc, const char* argv[])
+{
+	const int count = 32;
+
+    //Get the displays
+    auto displays = GetDisplays();
+
+    //Create a window
+    auto window = CreateWindow("Instanced Geometry", 0, 0, 1280, 720, displays[0], WINDOW_CENTER | WINDOW_TITLEBAR);
+
+	//Create framebuffer
+	auto framebuffer = CreateFramebuffer(window);
+
+    //Create world
+    auto world = CreateWorld();
+
+	//Create camera
+	auto camera = CreateCamera(world);
+	camera->SetPosition(0, 0, -count * 2);
+	camera->SetClearColor(0.25);
+
+	//Create box
+	auto box = CreateBox(world);
+	box->SetCollider(NULL);
+
+	//Create instances
+	std::vector<shared_ptr<Entity> > boxes;
+	boxes.reserve(count * count * count);
+	int x, y, z;
+	for (x = 0; x < 32; ++x)
+	{
+		for (y = 0; y < 32; ++y)
+		{
+			for (z = 0; z < 32; ++z)
+			{
+				auto inst = box->Instantiate(world);
+				inst->SetPosition(3.0f + float(x - 1 - (count / 2)) * 2, 3.0f + float(y - 1 - (count / 2)) * 2, 3.0f + float(z - 1 - (count / 2)) * 2);
+				boxes.push_back(inst);
+			} 
+		}
+	}
+	
+	box = NULL;
+
+    //Main loop
+    while (window->Closed() == false and window->KeyDown(KEY_ESCAPE) == false)
+    {
+        world->Update();
+        world->Render(framebuffer, false);
+    }
+    return 0;
+}
---
+++ Project Files/Ultra Engine/Source/UltraEngine.cpp
@@ -0,0 +1,1 @@
+#include "UltraEngine.h"
